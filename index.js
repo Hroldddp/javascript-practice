0c6fa8ecea7e7ef6aa674e177c38b52174cccead
Hroldddp <121552846+Hroldddp@users.noreply.github.com>
2024-08-31 11:20:32 +0800

    Final

---
+++ index.js
@@ -0,0 +1,27 @@
+let amount = 0;
+function add() {
+    document.getElementById("result").innerHTML = `Amount: ${amount + 1}`;
+    amount = amount + 1;
+}
+
+function add2() {
+    document.getElementById("result").innerHTML = `Amount: ${amount + 2}`;
+    amount = amount + 2;
+}
+
+function remove() {
+    amount = amount - 1;
+    if (amount > 0) {
+        document.getElementById("result").innerHTML = `Amount: ${amount}`;
+    }
+
+    else if (amount <= 0) {
+        amount = 0;
+        document.getElementById("result").innerHTML = `Amount: ${amount}`;
+    }
+}
+
+function reset() {
+    amount = 0;
+    document.getElementById("result").innerHTML = `Amount: ${amount}`;
+}
